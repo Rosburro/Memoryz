2d73fb0f234b9bdd247f026998a5dee09704bf45
itteB <126148750+itteB@users.noreply.github.com>
2024-04-21 19:57:30 +0200

    Update user.php
    aggiunta delle funzioni mancanti (manca qualche parte logica), miglioramento leggibilità, aggiunto variabili per img

--- progettoMemory/singlePlayer/user.php
+++ progettoMemory/singlePlayer/user.php
@@ -1,5 +1,23 @@
 <?php
+    
+    
+    //variabili prj finale
+    //    /*
+    $urlUnknown = "http://sitinosetosobellino.altervista.org/progettoMemory/img/unknown.png";
+    $urlImmagine = "http://sitinosetosobellino.altervista.org/progettoMemory/img/";
+    $memory = "../memory.xml";
+    //    */
 
+
+    //varibili in loco mio
+    /*
+    $memory = "memory.xml";
+    $urlImmagine = "img/";
+    $urlUnknown = "img/unknown.png";
+    */
+
+
+    //PRENDE IN GET IL N DI FIGURINE
     if(isset($_GET['quant'])){
         $quanteImmagini = $_GET['quant']; //prende quante img
         session_start();
@@ -10,33 +28,10 @@
         $quanteImmagini = 8;    //consiglio max 16 (16 immagini + 16 consigli)
     }
 
-    $memory = "../memory.xml";
+
+
+    //PARTE ARRAY
     $arrayXML = simplexml_load_file($memory);
-    
-
-
-    //echo inzializzazione pagine html,js,css
-    echo " 
-        <html>
-        <head>
-            <title> Memory </title>
-            <meta charset='utf-8'>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
-            <link rel='stylesheet' type='text/css' href='styles.css'>
-            <script type='text/javascript' src='js.js'></script>
-        </head>";
-
-    //impaginazione schermata utente con tabella immagini
-    echo"
-
-        <body onload='mescolaTabella(); salvaCont()'>
-            <form name='memory'>
-            <table align='center' id='tabella' cellpadding='10'>
-            
-
-    ";
-
     $array1 = json_decode(json_encode($arrayXML), true);    //conversione da array xml a array php
 
 
@@ -59,16 +54,49 @@
     }
 
     shuffle($personaggi);           //mescola l'array prima di mettere in tabella (per non dover mescolare le colonne della tabella)
+    
 
-    //TABELLA 4 IMG
+    //FINE ARRAY
+
+
     
 
 
-    if($quanteImmagini == 16){sediciPC($personaggi,$quanteImmagini);}
-    if($quanteImmagini == 4){quattro($personaggi,$quanteImmagini);}
-    if($quanteImmagini == 8){otto($personaggi,$quanteImmagini);}
-    if($quanteImmagini == 10){dieci($personaggi,$quanteImmagini);}
 
+
+    //echo inzializzazione pagine html,js,css
+    echo " 
+        <html>
+        <head>
+            <title> Memory </title>
+            <meta charset='utf-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+            <link rel='stylesheet' type='text/css' href='styles.css'>
+            <script type='text/javascript' src='js.js'></script>
+        </head>";
+
+
+    //impaginazione schermata utente con tabella immagini
+    echo"
+
+        <body onload='mescolaTabella(); salvaCont()'>
+            <form name='memory'>
+            <table align='center' id='tabella' cellpadding='10'>
+            
+
+    ";
+
+
+
+    //FUNZIONI DI STAMPA PER LE FIGURINE
+
+    if($quanteImmagini == 4){quattro($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine);}
+    if($quanteImmagini == 8){otto($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine);}
+    if($quanteImmagini == 10){dieci($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine);}
+    if($quanteImmagini == 12){dodici($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine);}
+    if($quanteImmagini == 14){quattordici($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine);}
+    if($quanteImmagini == 16){sedici($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine);}
 
 
 
@@ -82,7 +110,7 @@
 
     //FUNZIONI IN BASE AL N DI IMG
 
-    function sediciPC($personaggi,$quanteImmagini){
+    function sedici($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine){
         $count = 0;
         foreach ($personaggi as $persona) {
             if ($count == 0) { 
@@ -95,7 +123,7 @@
             echo "
                 <td><br>
                 
-                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='http://sitinosetosobellino.altervista.org/progettoMemory/img/unknown.png' width='250' height='400' >
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
                     <div hidden >
                     <img src='http://sitinosetosobellino.altervista.org/progettoMemory/img/$persona[img]' width='250' height='400' >
                     </div>
@@ -116,7 +144,7 @@
                 echo "</tr><tr>";
             }
             echo "<td> <br>
-                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='http://sitinosetosobellino.altervista.org/progettoMemory/img/unknown.png' width='250' height='400' >
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
                     
                     <div hidden >
                     <h1> $persona[n_completo]</h1> ";
@@ -132,17 +160,17 @@
     }
 
 
-    function quattro($personaggi,$quanteImmagini){
+    function quattro($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine){
         echo "<tr>";
         foreach ($personaggi as $persona) {
-            
+            $imgPers = $urlImmagine.$persona["img"];
             echo "
                 
                 <td><br>
                 
-                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='http://sitinosetosobellino.altervista.org/progettoMemory/img/unknown.png' width='250' height='400' >
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
                     <div hidden >
-                    <img src='http://sitinosetosobellino.altervista.org/progettoMemory/img/$persona[img]' width='250' height='400' >
+                    <img src='$imgPers' width='250' height='400' >
                     </div>
                 
                 </td>
@@ -157,7 +185,7 @@
         foreach ($personaggi as $persona) {
             
             echo "<td> <br>
-                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='http://sitinosetosobellino.altervista.org/progettoMemory/img/unknown.png' width='250' height='400' >
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
                     
                     <div hidden >
                     <h1> $persona[n_completo]</h1> ";
@@ -173,18 +201,19 @@
     }
 
 
-    function otto($personaggi,$quanteImmagini){
+    function otto($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine){
         echo "<tr>";
         $contatore = 1;
         foreach ($personaggi as $persona) {
+            $imgPers = $urlImmagine.$persona["img"];
             if($contatore>4){echo"</tr>"; $contatore=1;}
             echo "
                 
                 <td><br>
                 
-                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='http://sitinosetosobellino.altervista.org/progettoMemory/img/unknown.png' width='250' height='400' >
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
                     <div hidden >
-                    <img src='http://sitinosetosobellino.altervista.org/progettoMemory/img/$persona[img]' width='250' height='400' >
+                    <img src='$imgPers' width='250' height='400' >
                     </div>
                 
                 </td>
@@ -200,7 +229,7 @@
         foreach ($personaggi as $persona) {
             if($contatore>4){echo"</tr>"; $contatore=1;}
             echo "<td> <br>
-                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='http://sitinosetosobellino.altervista.org/progettoMemory/img/unknown.png' width='250' height='400' >
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
                     
                     <div hidden >
                     <h1> $persona[n_completo]</h1> ";
@@ -216,18 +245,20 @@
     }
 
 
-    function dieci($personaggi,$quanteImmagini){
+    function dieci($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine){
         echo "<tr>";
         $contatore = 1;
         foreach ($personaggi as $persona) {
+            $imgPers = $urlImmagine.$persona["img"];
+
             if($contatore>5){echo"</tr>"; $contatore=1;}
             echo "
                 
                 <td><br>
                 
-                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='http://sitinosetosobellino.altervista.org/progettoMemory/img/unknown.png' width='250' height='400' >
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
                     <div hidden >
-                    <img src='http://sitinosetosobellino.altervista.org/progettoMemory/img/$persona[img]' width='250' height='400' >
+                    <img src='$imgPers' width='250' height='400' >
                     </div>
                 
                 </td>
@@ -243,7 +274,51 @@
         foreach ($personaggi as $persona) {
             if($contatore>5){echo"</tr>"; $contatore=1;}
             echo "<td> <br>
-                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='http://sitinosetosobellino.altervista.org/progettoMemory/img/unknown.png' width='250' height='400' >
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
+                    
+                    <div hidden >
+                    <h1> $persona[n_completo]</h1> ";
+            foreach ($persona["sugg"] as $consiglio){
+                echo "<p>$consiglio</p>";
+            }   
+            echo "
+                    </div>    
+                    </td>";     
+            $contatore++;
+        }
+        echo "</tr></table>";
+    }
+
+    function dodici($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine){
+        echo "<tr>";
+        $contatore = 1;
+        foreach ($personaggi as $persona) {
+            $imgPers = $urlImmagine.$persona["img"];
+
+            if($contatore>6){echo"</tr>"; $contatore=1;}
+            echo "
+                
+                <td><br>
+                
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
+                    <div hidden >
+                    <img src='$imgPers' width='250' height='400' >
+                    </div>
+                
+                </td>
+                ";
+            $contatore++;
+        }
+        echo "</tr>";
+
+        //stampa tutte le figurine info
+        shuffle($personaggi);
+        echo "<tr>";
+        $contatore = 1;
+        foreach ($personaggi as $persona) {
+            if($contatore>6){echo"</tr>"; $contatore=1;}
+            echo "<td> <br>
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
                     
                     <div hidden >
                     <h1> $persona[n_completo]</h1> ";
@@ -259,6 +334,48 @@
     }
 
 
-?>
+    function quattordici($personaggi,$quanteImmagini,$urlUnknown,$urlImmagine){
+        echo "<tr>";
+        $contatore = 1;
+        foreach ($personaggi as $persona) {
+            $imgPers = $urlImmagine.$persona["img"];
 
-                            
+            if($contatore>7){echo"</tr>"; $contatore=1;}
+            echo "
+                
+                <td><br>
+                
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
+                    <div hidden >
+                    <img src='$imgPers' width='250' height='400' >
+                    </div>
+                
+                </td>
+                ";
+            $contatore++;
+        }
+        echo "</tr>";
+
+        //stampa tutte le figurine info
+        shuffle($personaggi);
+        echo "<tr>";
+        $contatore = 1;
+        foreach ($personaggi as $persona) {
+            if($contatore>7){echo"</tr>"; $contatore=1;}
+            echo "<td> <br>
+                    <img id='immagineUnknown' onclick='giraFigurina(this,$persona[indice])' src='$urlUnknown' width='250' height='400' >
+                    
+                    <div hidden >
+                    <h1> $persona[n_completo]</h1> ";
+            foreach ($persona["sugg"] as $consiglio){
+                echo "<p>$consiglio</p>";
+            }   
+            echo "
+                    </div>    
+                    </td>";     
+            $contatore++;
+        }
+        echo "</tr></table>";
+    }
+
+?>                       
